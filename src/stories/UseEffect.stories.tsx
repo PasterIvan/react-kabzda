cecd1b544714db667c62100795c25492c719815d
Ivan <paster-ivan@yandex.by>
2022-06-08 11:56:37 +0300

    UseEffect.stories.tsx

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -35,3 +35,21 @@ export const SimpleExample = () => {
     </>
 }
 
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    console.log('SimpleExample')
+
+    useEffect(()=> {
+        setInterval(()=>{
+        setCounter(state=>state+1)
+    }, 1000)
+    }, [])
+
+
+    return <>
+        Hello, counter:{counter} fake:{fake}
+    </>
+}
+
